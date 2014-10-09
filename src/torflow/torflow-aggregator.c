6bb7bd41c76b07308ef2787739329115a9cc7276
aaron-segal <aaron.segal@yale.edu>
2014-10-09 18:35:45 -0400

    sweet new symlink feature added

--- src/torflow/torflow-aggregator.c
+++ src/torflow/torflow-aggregator.c
@@ -7,10 +7,11 @@
 struct _TorFlowAggregator {
 	ShadowLogFunc slogf;
 	gint numWorkers;
-	gboolean* seenWorkers;
+	gboolean gotInitial;
 	GString* filename;
 	GHashTable* relayStats;
 	gdouble nodeCap;
+	gint version;
 };
 
 typedef struct _TorFlowRelayStats {
@@ -63,13 +64,18 @@ static void _torflowaggregator_printToFile(TorFlowAggregator* tfa) {
 		totalBW += current->newBandwidth;
 	}
 
-	//loop through nodes and cap bandwidths that are too large, then print to file
+	//create new file to print to, and increment version
+	GString* newFilename = g_string_new(tfa->filename->str);
+	g_string_append_printf(newFilename, ".%d", tfa->version++);
 	struct timespec now_ts;
 	clock_gettime(CLOCK_REALTIME, &now_ts);
 	FILE * fp;
-	fp = fopen(tfa->filename->str, "w");
+	fp = fopen(newFilename->str, "w");
 	fprintf(fp, "%li\n", now_ts.tv_sec);
 
+
+
+	//loop through nodes and cap bandwidths that are too large, then print to file
 	g_hash_table_iter_init(&iter, tfa->relayStats);
 	while(g_hash_table_iter_next(&iter, &key, &value)) {
 		TorFlowRelayStats* current = (TorFlowRelayStats*)value;
@@ -86,13 +92,50 @@ static void _torflowaggregator_printToFile(TorFlowAggregator* tfa) {
 				current->nickname->str);
 	}
 	fclose(fp);
+	if(!unlink(tfa->filename->str)) {
+		tfa->slogf(SHADOW_LOG_LEVEL_WARNING, __FUNCTION__,
+				"Unable to remove symlink to %s!\n",
+				tfa->filename->str);
+	}
+	if(!symlink(newFilename->str, tfa->filename->str)) {
+		tfa->slogf(SHADOW_LOG_LEVEL_WARNING, __FUNCTION__,
+				"Unable to create symlink from %s to %s!\n",
+				newFilename->str, tfa->filename->str);
+	}
+	g_string_free(newFilename, TRUE);
 }
 
-void torflowaggregator_reportMeasurements(TorFlowAggregator* tfa, GSList* measuredRelays, gint workerID) {
+void torflowaggregator_reportInitial(TorFlowAggregator* tfa, GSList* relays) {
+	g_assert(tfa);
+
+	//We only need to populate with initial data once.
+	if(tfa->gotInitial) {
+		return;
+	} else {
+		tfa->gotInitial = TRUE;
+	}
+	
+	//add all relays that the worker measured to our stats list
+	for(GSList* currentNode = relays; currentNode; currentNode = g_slist_next(currentNode)) {
+		TorFlowRelay* current = currentNode->data;
+		TorFlowRelayStats* tfrs = g_new0(TorFlowRelayStats, 1);
+		tfrs->nickname = g_string_new(current->nickname->str);
+		tfrs->identity = g_string_new(current->identity->str);
+		tfrs->descriptorBandwidth = current->descriptorBandwidth;
+		tfrs->advertisedBandwidth = current->advertisedBandwidth;
+		tfrs->meanBandwidth = current->descriptorBandwidth;
+		tfrs->filteredBandwidth = current->descriptorBandwidth;
+		g_hash_table_insert(tfa->relayStats, tfrs->identity->str, tfrs);
+	}
+}
+
+void torflowaggregator_reportMeasurements(TorFlowAggregator* tfa, GSList* measuredRelays, gint sliceSize, gint currSlice) {
 	g_assert(tfa);
 	
 	//add all relays that the worker measured to our stats list
-	for(GSList* currentNode = measuredRelays; currentNode; currentNode = g_slist_next(currentNode)) {
+	GSList* currentNode = g_slist_nth(measuredRelays, sliceSize * currSlice);
+	gint i = 0;
+	while (currentNode && i < sliceSize) {
 		TorFlowRelay* current = currentNode->data;
 		if (current->measureCount >= MEASUREMENTS_PER_SLICE) {
 			TorFlowRelayStats* tfrs = g_new0(TorFlowRelayStats, 1);
@@ -104,31 +147,18 @@ void torflowaggregator_reportMeasurements(TorFlowAggregator* tfa, GSList* measur
 			tfrs->filteredBandwidth = torflowutil_filteredBandwidth(current, tfrs->meanBandwidth);
 			g_hash_table_insert(tfa->relayStats, tfrs->identity->str, tfrs);
 		}
-	}
-	tfa->seenWorkers[workerID] = TRUE;
-	
-	//Check to see if we've gotten everything we need to print to file
-	gboolean seenAll = TRUE;
-	for(gint i = 0; i < tfa->numWorkers; i++) {
-		seenAll = seenAll && tfa->seenWorkers[i];
+		currentNode = g_slist_next(currentNode);
+		i++;
 	}
 
-	//If so, print and reset for next group of measurements
-	if(seenAll) {
-		_torflowaggregator_printToFile(tfa);
-		g_hash_table_destroy(tfa->relayStats);
-		tfa->relayStats = g_hash_table_new_full(g_str_hash, g_str_equal, NULL, _torflowaggregator_torFlowRelayStatsFree);
-		for(gint i = 0; i < tfa->numWorkers; i++) {
-			tfa->seenWorkers[i] = FALSE;
-		}
-	}
+	//print results to file	
+	_torflowaggregator_printToFile(tfa);
 }
 
 void torflowaggregator_free(TorFlowAggregator* tfa) {
 	g_assert(tfa);
 
 	g_hash_table_destroy(tfa->relayStats);
-	g_free(tfa->seenWorkers);
 
 	g_string_free(tfa->filename, TRUE);
 	g_free(tfa);
@@ -143,8 +173,9 @@ TorFlowAggregator* torflowaggregator_new(ShadowLogFunc slogf,
 	tfa->numWorkers = numWorkers;
 	tfa->filename = g_string_new(filename);
 	tfa->nodeCap = nodeCap;
+	tfa->version = 0;
 	tfa->relayStats = g_hash_table_new_full(g_str_hash, g_str_equal, NULL, _torflowaggregator_torFlowRelayStatsFree);
-	tfa->seenWorkers = g_new0(gboolean, tfa->numWorkers);
+	tfa->gotInitial = FALSE;
 
 	return tfa;
 }
