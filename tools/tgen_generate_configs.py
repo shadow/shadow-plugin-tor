972d92d0e3f9d5296ae1bad07018cee231269892
Rob Jansen <jansen@cs.umn.edu>
2014-11-22 08:19:04 -0500

    tools to help switch to new tgen client

---
+++ tools/tgen_generate_configs.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python
+
+import sys, networkx as nx
+
+NUMSERVERS=30
+
+def main():
+    servers = []
+    for i in xrange(NUMSERVERS): servers.append("server{0}:80".format(i+1))
+    s = ','.join(servers)
+
+    generate_tgen_server()
+    generate_tgen_filetransfer_clients(servers=s)
+    generate_tgen_perf_clients(servers=s, size="50 KiB", name="tgen.torperfclient50k.graphml.xml")
+    generate_tgen_perf_clients(servers=s, size="1 MiB", name="tgen.torperfclient1m.graphml.xml")
+    generate_tgen_perf_clients(servers=s, size="5 MiB", name="tgen.torperfclient5m.graphml.xml")
+
+def generate_tgen_server():
+    G = nx.DiGraph()
+    G.add_node("start", serverport="80")
+    nx.write_graphml(G, "tgen.server.graphml.xml")
+
+def generate_tgen_filetransfer_clients(servers):
+    # webclients
+    G = nx.DiGraph()
+
+    G.add_node("start", socksproxy="localhost:9000", serverport="8888", peers=servers)
+    G.add_node("transfer", type="get", protocol="tcp", size="320 KiB")
+    G.add_node("pause", time="1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60")
+
+    G.add_edge("start", "transfer")
+    G.add_edge("transfer", "pause")
+    G.add_edge("pause", "start")
+
+    nx.write_graphml(G, "tgen.torwebclient.graphml.xml")
+
+    # bulkclients
+    G = nx.DiGraph()
+
+    G.add_node("start", socksproxy="localhost:9000", serverport="8888", peers=servers)
+    G.add_node("transfer", type="get", protocol="tcp", size="5 MiB")
+
+    G.add_edge("start", "transfer")
+    G.add_edge("transfer", "start")
+
+    nx.write_graphml(G, "tgen.torbulkclient.graphml.xml")
+
+def generate_tgen_perf_clients(servers="server1:8888,server2:8888", size="50 KiB", name="tgen.perfclient50k.graphml.xml"):
+    G = nx.DiGraph()
+
+    G.add_node("start", socksproxy="localhost:9000", serverport="8888", peers=servers)
+    G.add_node("transfer", type="get", protocol="tcp", size=size)
+    G.add_node("pause", time="60")
+
+    G.add_edge("start", "transfer")
+    G.add_edge("transfer", "pause")
+    G.add_edge("pause", "start")
+
+    nx.write_graphml(G, name)
+
+if __name__ == '__main__': sys.exit(main())
---
+++ tools/tgen_replace_filetransfer.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python
+
+"""
+takes a shadow.config.xml file and replaces all uses of the filetransfer
+plugin with the tgen plugin. the configs used here can be generated with
+'tgen_generate_configs.py'.
+"""
+
+import sys
+from lxml import etree
+
+if len(sys.argv) != 3: print >>sys.stderr, "{0} input.xml output.xml".format(sys.argv[0]);exit()
+
+infname = sys.argv[1]
+outfname = sys.argv[2]
+
+parser = etree.XMLParser(remove_blank_text=True)
+tree = etree.parse(infname, parser)
+root = tree.getroot()
+
+for p in root.iterchildren("plugin"):
+    if 'filetransfer' in p.get("id"):
+        p.set("id", "tgen")
+        p.set("path", "~/.shadow/plugins/libshadow-plugin-tgen.so")
+
+hastgen = False        
+for p in root.iterchildren("plugin"):
+    if 'tgen' in p.get("id"): hastgen = True
+assert hastgen == True
+
+for n in root.iterchildren("node"):
+    for a in n.iterchildren("application"):
+        if 'filetransfer' in a.get("plugin"):
+            starttime = a.get("starttime")
+            if starttime is None: starttime = a.get("time")
+            n.remove(a)
+            a = etree.SubElement(n, "application")
+            a.set("plugin", "tgen")
+            a.set("starttime", "{0}".format(starttime))
+            if 'server' in n.get("id"):
+                a.set("arguments", "tgen.server.graphml.xml")
+            elif 'webclient' in n.get("id"):
+                a.set("arguments", "tgen.torwebclient.graphml.xml")
+            elif 'bulkclient' in n.get("id"):
+                a.set("arguments", "tgen.torbulkclient.graphml.xml")
+            elif 'perfclient50k' in n.get("id"):
+                a.set("arguments", "tgen.torperfclient50k.graphml.xml")
+            elif 'perfclient1m' in n.get("id"):
+                a.set("arguments", "tgen.torperfclient1m.graphml.xml")
+            elif 'perfclient5m' in n.get("id"):
+                a.set("arguments", "tgen.torperfclient5m.graphml.xml")
+            else: assert False
+
+with open(outfname, 'wb') as outf:
+    print >>outf, (etree.tostring(root, pretty_print=True, xml_declaration=False))
+
