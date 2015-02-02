edafc8c6ee2b8af9bf7034efa5e4832b022e862e
Rob Jansen <jansen@cs.umn.edu>
2015-02-02 16:51:59 -0500

    rename scallion to tor in config file

--- tools/convert_1.9.2_to_1.10.0.sh
+++ tools/convert_1.9.2_to_1.10.0.sh
@@ -4,6 +4,7 @@
 
 rm *dl *dat
 python tgen_generate_configs.py
+sed -i 's/scallion/tor/g' shadow.config.xml
 python tgen_replace_filetransfer.py shadow.config.xml shadow.config.xml
 python tor_convert_args.py shadow.config.xml shadow.config.xml
 python tor_create_v3bw.py
