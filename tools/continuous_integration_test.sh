86e9f9a4f1eb8a469959f1a4d8639d60066b024f
Jim Newsome <jnewsome@torproject.org>
2020-02-20 07:33:30 -0800

    Run each CI step in a subshell.
    This provides stronger and more portable isolation between steps than
    e.g. 'local' and 'pushd/popd'.

--- tools/continuous_integration_test.sh
+++ tools/continuous_integration_test.sh
@@ -14,16 +14,16 @@
 
 set -euo pipefail
 
-install_compiler () {
+install_compiler () (
     echo $FUNCNAME $@
     case "$1" in
       gcc)   sudo apt-get install -y gcc g++ ;;
       clang) sudo apt-get install -y clang ;;
       *) echo "bad cc=$1" && exit 1 ;;
     esac
-}
+)
 
-install_shadow_build_deps () {
+install_shadow_build_deps () (
     echo $FUNCNAME $@
     sudo apt-get install -y \
         cmake \
@@ -36,17 +36,16 @@ install_shadow_build_deps () {
         libigraph0-dev \
         libc-dbg \
         python-pyelftools
-}
+)
 
-install_shadow () {
+install_shadow () (
     echo $FUNCNAME $@
-    pushd $1
+    cd $1
     ./setup build
     ./setup install
-    popd
-}
+)
 
-install_tgen_build_deps () {
+install_tgen_build_deps () (
     echo $FUNCNAME $@
     sudo apt-get install -y \
         cmake \
@@ -54,19 +53,18 @@ install_tgen_build_deps () {
         libglib2.0-dev \
         libigraph0-dev \
         libigraph0v5
-}
+)
 
-install_tgen () {
+install_tgen () (
     echo $FUNCNAME $@
-    pushd $1
+    cd $1
     mkdir -p build
     cd build
     cmake .. -DCMAKE_INSTALL_PREFIX=$2
     make install
-    popd
-}
+)
 
-install_shadow_plugin_tor_build_deps () {
+install_shadow_plugin_tor_build_deps () (
     echo $FUNCNAME $@
     sudo apt-get install -y \
         autoconf \
@@ -74,52 +72,49 @@ install_shadow_plugin_tor_build_deps () {
         liblzma5 \
         liblzma-dev \
         zlib1g-dev
-}
+)
 
-install_shadow_plugin_tor () {
+install_shadow_plugin_tor () (
     echo $FUNCNAME $@
-    pushd $1
+    cd $1
     ./setup dependencies -y
     ./setup build -y
     ./setup install
-    popd
-}
+)
 
-run_simulation () {
+run_simulation () (
     echo $FUNCNAME $@
-    local SIMULATION_DIR=$1
-    local SHADOW_PLUGIN_TOR_SRC=$2
-    local SHADOW_BIN=$3
+    SIMULATION_DIR=$1
+    SHADOW_PLUGIN_TOR_SRC=$2
+    SHADOW_BIN=$3
 
     mkdir -p $SIMULATION_DIR
-    pushd $SIMULATION_DIR
+    cd $SIMULATION_DIR
 
     tar xaf $SHADOW_PLUGIN_TOR_SRC/resource/shadowtor-minimal-config.tar.xz
     mv shadowtor-minimal-config shadowtor-minimal
     cd shadowtor-minimal
     $SHADOW_BIN shadow.config.xml > shadow.log
+)
 
-    popd
-}
-
-validate_simulation () {
+validate_simulation () (
     echo $FUNCNAME $@
     SIMULATION_DIR=$1
 
-    local COMPLETE=`find $SIMULATION_DIR/shadowtor-minimal/shadow.data/hosts -name '*client*.log' -exec grep transfer-complete \{\} \; | wc -l`
+    COMPLETE=`find $SIMULATION_DIR/shadowtor-minimal/shadow.data/hosts -name '*client*.log' -exec grep transfer-complete \{\} \; | wc -l`
     if [ $COMPLETE -ne 40 ]
     then
       echo Expected 40 transfers got $COMPLETE
       return 1
     fi
-}
+)
 
-main () {
-    local INSTALL_PREFIX=$HOME/.shadow
-    local TGEN_SRC=$PWD/tgen
-    local SHADOW_SRC=$PWD/shadow
-    local SHADOW_PLUGIN_TOR_SRC=$PWD/shadow-plugin-tor
-    local SIMULATION_DIR=$PWD/simulation
+main () (
+    INSTALL_PREFIX=$HOME/.shadow
+    TGEN_SRC=$PWD/tgen
+    SHADOW_SRC=$PWD/shadow
+    SHADOW_PLUGIN_TOR_SRC=$PWD/shadow-plugin-tor
+    SIMULATION_DIR=$PWD/simulation
 
     install_compiler $CC
 
@@ -134,7 +129,7 @@ main () {
     
     run_simulation $SIMULATION_DIR $SHADOW_PLUGIN_TOR_SRC $INSTALL_PREFIX/bin/shadow
     validate_simulation $SIMULATION_DIR
-}
+)
 
 main
 
